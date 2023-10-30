7100031d305f33eb9b2ac7f8692b38d2e48ab530
Rana-az <140128656+Rana-az@users.noreply.github.com>
2023-10-30 17:48:05 +0300

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
   		<button type="submit" class="btn" name="login_user">Login</button>
   	</div>
   	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
+  		Not yet a member? <a href="register.php">Sign up</button>
   	</p>
   </form>
 </body>
